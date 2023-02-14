ba996cbeff580c9ba4f3a8f2e43a5fa6fc6b6ddc
nazarkuhta <nazar.kuhta.2004@gmail.com>
2023-02-14 10:09:53 +0200

    hw4

--- js-hw/hw4/hw4.js
+++ js-hw/hw4/hw4.js
@@ -127,6 +127,17 @@
 
 // - створити функцію яка повертає найменьше число з масиву
 
+// function minNum(numbers){
+//     let min = numbers[0]
+//     for (let number of numbers) {
+//         if (min > number){
+//             min = number
+//         }
+//     }
+//     return min
+// }
+// console.log(minNum([-1,1,-10,23]))
+
 
 
 
@@ -150,6 +161,27 @@
 // Приклад  swap([11,22,33,44],0,1) //=> [22,11,33,44]
 
 
+// function swap(arr, index1, index2) {
+//     let n1 = arr[index1]
+//     let n2 = arr[index2]
+//
+//     arr[index1] = n2
+//     arr[index2] = n1
+// }
+
 
 // - Написати функцію обміну валюти exchange(sumUAH,currencyValues,exchangeCurrency)
 // Приклад exchange(10000,[{currency:'USD',value:40},{currency:'EUR',value:42}],'USD') // => 250
+
+
+
+
+// function exchange(sumUAH,currencyValues,exchangeCurrency){
+//
+//     for (let currencyValue of currencyValues) {
+//         if (currencyValue.currency === exchangeCurrency){
+//             return sumUAH/ currencyValue.value
+//         }
+//     }
+// }
+// console.log(exchange(10000,[{currency:'USD',value:40},{currency:'EUR',value:42}],'USD'))
