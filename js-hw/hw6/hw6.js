07602a5cd217da889784e876ba2de4eb2c6da69a
nazarkuhta <nazar.kuhta.2004@gmail.com>
2023-02-20 19:42:40 +0200

    hw6

--- js-hw/hw6/hw6.js
+++ js-hw/hw6/hw6.js
@@ -35,7 +35,7 @@
 
 // let str = ' dirty string   '
 //
-// console.log(str.replace(`  `,  ``).replace(` ` , ``))
+// console.log(str.trim())
 
 // - Напишіть функцію stringToarray(str), яка перетворює рядок на масив слів.
 //     let str = 'Ревуть воли як ясла повні';
